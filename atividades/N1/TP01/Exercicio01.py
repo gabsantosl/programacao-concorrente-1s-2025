86775baa40941fe7e9e459d734d685e7347848ce
Gabriela Santos <gabsantosleite@gmail.com>
2025-03-25 21:41:16 -0300

    exercicio 02

--- atividades/N1/TP01/Exercicio01.py
+++ atividades/N1/TP01/Exercicio01.py
@@ -1,6 +1,18 @@
+import threading 
 import random
 
-# Função principal do QuickSort
+# Função principal do QuickSort 
+'''
+def: criação de uma função
+arr: array -> estrutura de dados que armazena uma coleção de elementos do mesmo tipo
+pivot: valor a ser armazenado para uso em uma função. Nesse caso,
+o primeiro pivot é o ultimo numero a ser gerado ([-1] -> inverte)
+a ordenação dos algoritmos acontece a partir do valor do pivot.
+ex: ultimo numero gerado foi 29, pivot = 29, 40 > 29 logo vai para a direita
+21 < 29 logo vai para a esquerda.
+A palavra *args (abreviação de arguments ) é utilizada quando precisamos passar vários 
+valores para função sem saber ao certo quantos serão
+'''
 
 def quicksort(arr):
     if len(arr) <= 1:
@@ -12,7 +24,7 @@ def quicksort(arr):
 
 # Função para gerar números aleatórios
 
-def gerar_numeros_aleatorios(n=100, min_val=1, max_val=200):
+def gerar_numeros_aleatorios(n=10, min_val=1, max_val=200):
     return [random.randint(min_val, max_val) for _ in range(n)]
 
 # Função principal para testar o QuickSort
